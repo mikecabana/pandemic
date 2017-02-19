5ba8a73479e6eda88a5b10748fe37c1b9f231263
mikecabana <mikeyc343@gmail.com>
2017-02-19 03:36:56 -0500

    wip 02/19/2017

--- Pandemic/main.cpp
+++ Pandemic/main.cpp
@@ -9,7 +9,15 @@
 int main() {
 	//testing city card class
 	PlayerCard *city(new City("mtl", "blue", 1000000));
+	PlayerCard *city2(new City("tor", "blue", 1000000));
+	PlayerCard *city3(new City("lav", "blue", 1000000));
+	PlayerCard *city4(new City("que", "blue", 1000000));
+	PlayerCard *city5(new City("ota", "blue", 1000000));
 	city->getAttributes();
+	city2->getAttributes();
+	city3->getAttributes();
+	city4->getAttributes();
+	city5->getAttributes();
 	std::cout << std::endl;
 
 	//testing epidemic card class
@@ -22,17 +30,15 @@ int main() {
 	eve->getAttributes();
 	std::cout << std::endl;
 
+	//testing vector with inheritance
 	std::vector<PlayerCard> test;
 	test.push_back(*city);
-	test.data()->getAttributes();
-
-	std::vector<PlayerCard>::iterator it;
-
-	for (it = test.begin(); it != test.end(); ++it) {
-		std::cout << "t" << std::endl;
-		it->getAttributes();
-	}
-
+	test.push_back(*city2);
+	test.push_back(*city3);
+	test.push_back(*city4);
+	test.push_back(*city5);
+	test[0].getAttributes();
+	test[1].getAttributes();
 
 	return 0;
 }
